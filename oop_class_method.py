31b7bb7b7a95c300f38925a90f3a5e8c7bbd1a86
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-03-30 10:37:42 +0530

    Update oop_class_method.py
    OOP

--- oop_class_method.py
+++ oop_class_method.py
@@ -24,7 +24,7 @@ p3 = Person("Anshu", "Data", 1)
 print(Person.count_instance())
 
 # Class Method as a Constructor
-class Person:
+class Person1:
     instance_counter = 0
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -39,6 +39,26 @@ class Person:
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
     
-human = Person.from_string("Arpit,Gupta,24")
-
+human = Person1.from_string("Arpit,Gupta,24")
 print(human.full_name())
+
+# Static Method: It has no connection to class or Instance.
+class Person2:
+    instance_counter = 0
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        
+    @staticmethod
+    def hello():
+        return "\nHello I am Static Method  I have some logical connection to Your Class"
+    
+    @classmethod
+    def from_string(cls, string):
+        first,last,age = string.split(',')
+        return cls(first, last, int(age))
+    
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+print(Person2.hello())
