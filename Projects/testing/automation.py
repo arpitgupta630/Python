a5966eefa8fb0c124052c8751c5695d8fbfa49a0
Arpit Gupta <78097741+arpitgupta630@users.noreply.github.com>
2022-05-18 19:11:27 +0530

    url

--- Projects/testing/automation.py
+++ Projects/testing/automation.py
@@ -3,3 +3,5 @@ from selenium import webdriver
 chrome_browser = webdriver.Chrome("./chromedriver.exe")
 
 chrome_browser.maximize_window()
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+assert "Selenium Easy Demo" in chrome_browser.title
